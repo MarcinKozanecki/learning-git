9e7722df82136d69735cb9374a694be30add94aa
Marcin Kozanecki. <m_kozanecki@wp.pl>
2025-01-17 13:59:52 +0100

    pozdrowienia w task.py

--- task.py
+++ task.py
@@ -1,11 +1,1 @@
-shopping_list={
-    "piekarnia": ["chleb", "bułki", "pączki"],
-    "warzywniak":["marchew", "seler","rukola", "truskawki"],
-    "sportowy":["kask", "termo","kurtka"],
-}
-
-total_product=0
-for shop in shopping_list:
-    print("Ide do sklepu", shop.title(),"i kupuje tu następujące rzeczy", [product.title() for product in shopping_list[shop]])
-    total_product+=len(shopping_list[shop])
-print("w sumie kupuje", total_product,"produktów")
+print("Pozdrawiam mojego mentora")
